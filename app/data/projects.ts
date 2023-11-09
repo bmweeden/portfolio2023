6a859258ba8a2ae64318ccf1e378ebfe874a7b09
Ben Weeden <ben@benweeden.com>
2023-11-09 16:32:23 +0000

    wip

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -14,24 +14,124 @@ export const client_projects = [
       '/img/ebay/5.jpg',
     ],
   },
-  // {
-  //   client: 'Adidas',
-  //   slug: 'adidas',
-  //   description: 'Project 2 description',
-  // },
-  // {
-  //   client: 'BRC',
-  //   slug: 'BRC',
-  //   description: 'Project 3 description',
-  // },
-  // {
-  //   client: 'Event Disco',
-  //   slug: 'event-disco',
-  //   description: 'Project 4 description',
-  // },
-  // {
-  //   client: 'Tide',
-  //   slug: 'tide',
-  //   description: 'Project 5 description',
-  // },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
+  {
+    client: 'eBay',
+    slug: 'ebay',
+    description: `<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos est facere mollitia quos! Quod, veritatis magni! Veritatis doloremque dolorum, quod quibusdam, ad dicta distinctio labore soluta, possimus voluptatibus eaque laboriosam?</p>`,
+    images: [
+      '/img/ebay/1.jpg',
+      '/img/ebay/2.jpg',
+      '/img/ebay/3.jpg',
+      '/img/ebay/4.jpg',
+      '/img/ebay/5.jpg',
+    ],
+  },
 ];
